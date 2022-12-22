4fe5f14433f4063acf557e05d53fef3f9a1701ec
qa-exercises <118732445+qa-exercises@users.noreply.github.com>
2022-12-22 01:27:10 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-demo-props-are-like-html-attributes
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-demo-props-are-like-html-attributes)
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.css'
+
+export default function App() {
+  return (
+    <img
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png"
+      alt="React logo"
+      width={512}
+      height={445}
+    />
+  )
+}
